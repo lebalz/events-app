fb4e5c95d6152a65da0b6571228a043bd5af626c
LeBalz <lebalz@outlook.com>
2023-05-16 10:54:32 +0200

    fix gantt horizontal scrolling

--- src/pages/gantt.tsx
+++ src/pages/gantt.tsx
@@ -6,9 +6,11 @@ import "gantt-task-react/dist/index.css";
 import { observer } from 'mobx-react-lite';
 import { useStore } from '../stores/hooks';
 import Layout from '@theme/Layout';
+import EventModal from '../components/Event/Modal';
 
 const GanttView = observer(() => {
     const viewStore = useStore('viewStore');
+    const ref = React.useRef<HTMLDivElement>(null);
     const tasks: Task[] = (viewStore.semester?.events || []).filter((e) => e.isValid).map((e, idx) => {
         return {
             start: e.start,
@@ -31,10 +33,35 @@ const GanttView = observer(() => {
         }, 20);
         return () => clearTimeout(ts);
     }, [tasks]);
+
+    /**
+     * gantt-task-react handles (and prevents sometimes) horizontal scrolling with the mouse wheel.
+     * This is a workaround to enable horizontal scrolling.
+     * It depends on the order of event handlers, that the last added handlers will process first. If this changes,
+     * this workaround wont work anymore.
+     */
+    React.useEffect(() => {
+        const gantt = ref.current;
+        if (gantt) {
+            const children = gantt.querySelectorAll('div');
+            if (children.length > 1) {
+                /** the second div is the one that blocks the wheel event */
+                const wheelBlocker = children[1];
+                const onWheel = (e: WheelEvent) => {
+                    /** stops propagation to other handlers on this element, 
+                     * but does not stop propagation/bubbling  */
+                    e.stopImmediatePropagation();
+                }
+                wheelBlocker.addEventListener('wheel', onWheel);
+                return () => wheelBlocker.removeEventListener('wheel', onWheel);
+            }
+        }
+    }, [ref.current]);
+                
     return (
         <Layout>
             <div className={clsx(styles.container)}>
-                <div className={clsx(styles.gantt)}>
+                <div className={clsx(styles.gantt)} ref={ref}>
                     {tasks.length > 0 && (
                         <Gantt
                             tasks={tasks}
@@ -43,10 +70,14 @@ const GanttView = observer(() => {
                             viewDate={new Date()}
                             rowHeight={20}
                             locale="gsw"
+                            onClick={(task: Task) => {
+                                viewStore.setEventModalId(task.id);
+                            }}
                         />
                     )}
                 </div>
             </div>
+            <EventModal />
         </Layout>
     )
 });
