24c7e63ea727d43f7061090bbf96ddba58292f14
LeBalz <lebalz@outlook.com>
2023-06-26 21:25:03 +0200

    update table

--- src/pages/table.tsx
+++ src/pages/table.tsx
@@ -18,31 +18,62 @@ import Departments from '../components/Event/EventFields/Departments';
 import Klasses from '../components/Event/EventFields/Klasses';
 import styles from './table.module.scss';
 import { Icon, SIZE_S } from '../components/shared/icons';
+import Select from '../components/Event/EventFields/Select';
+import DepartmentsOrAudiencePicker from '../components/Event/EventFields/DepartmentsOrAudience';
+import {useWindowSize} from '@docusaurus/theme-common';
 
 const Table = observer(() => {
     const viewStore = useStore('viewStore');
     const eventStore = useStore('eventStore');
+    const windowSize = useWindowSize();
     return (
         <Layout>
             <div>
                 <EventGrid events={[]} groupBy='kw' showFilter />
                 <GridTable
                     className={styles.table}
-                    data={viewStore.eventTable.events}
+                    data={viewStore.semester?.events || []}
                     columns={{
-                        day: { maxWidth: '2.8em', label: 'Tag', transform: (item) => item.fStartDate, render: (item) => <Day event={item} /> },
-                        description: { maxWidth: '16em', label: 'Stichworte', render: (item) => <Description event={item} />},
+                        select: { 
+                            label: '', 
+                            render: (item, self) => (
+                                <Select 
+                                    event={item} 
+                                    onSelect={(selected: boolean, shiftKey: boolean) => {
+                                        const idx = self.items.findIndex(i => i.model.id === item.id);
+                                        const topIdx = self.items.slice(0, idx).findLastIndex(i => i.model.selected);
+                                        console.log(idx, topIdx);
+                                        if (shiftKey) {
+                                            if (topIdx > -1) {
+                                                self.items.slice(topIdx, idx).forEach(i => i.model.setSelected(selected));
+                                            }
+                                        }
+                                        item.setSelected(selected)
+                                    }}
+                            />),
+                            transform: (item) => item.id
+                        },
+                        day: { width: '2.8em', label: 'Tag', transform: (item) => item.fStartDate, render: (item) => <Day event={item} /> },
+                        description: { width: '16em', label: 'Stichworte', render: (item) => <Description event={item} />},
                         start: { label: 'Start', render: (item) => <DateTime event={item}  time='start'/>, className: styles.flex },
-                        end: { label: 'Ende', render: (item) => <DateTime event={item}  time='end'/> },
+                        end: { label: 'Ende', render: (item) => <DateTime event={item}  time='end'/>,  className: styles.flexEnd },
                         location: { label: 'Ort', render: (item) => <Location event={item} /> },
                         userGroup: { label: 'Gruppe', render: (item) => <UserGroup event={item} /> },
-                        departmens: { label: 'Abteilungen', transform: (item) => item.departmentNames.join(', '), render: (item) => <Departments event={item} /> },
-                        classes: { label: 'Klassen', transform: (item) => item.fClasses.map(t => t.text).join(', '), render: (item) => <Klasses event={item} /> },
-                        descriptionLong: {maxWidth: '20em', label: 'Beschreibung', render: (item) => <DescriptionLong event={item} /> },
+                        departmens: { label: 'Abteilungen', transform: (item) => item.departmentNames.join(', '), render: (item) => <DepartmentsOrAudiencePicker event={item} />, colSpan: (item) => item.isEditing ? 2 : 1 },
+                        classes: { label: 'Klassen', transform: (item) => item.fClasses.map(t => t.text).join(', '), render: (item) => <Klasses event={item} />, hidden: (item) => item.isEditing },
+                        descriptionLong: {width: '20em', label: 'Beschreibung', render: (item) => <DescriptionLong event={item} /> },
                         actions: { label:  <Icon path={mdiTools} size={SIZE_S} />, fixed: { right: 0 }, render: (item) => <Actions event={item} />, transform: (item) => item.id },
                     }}
                     groupBy='kw'
                     groupHeader={(item) => <div>KW {item.models[0].model.kw}</div>}
+                    onRowClick={(e, model) => {
+                        if (e.ctrlKey || e.metaKey || windowSize === 'desktop') {
+                        } else if (e.altKey && model.isEditable) {
+                            model.setEditing(true);
+                        } else {
+                            model.setExpanded(true);
+                        }
+                    }}
                />
                 <Button
                     onClick={() => {
