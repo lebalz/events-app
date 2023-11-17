ec6e00a57d4b9730cc91795b87c4d0bd025424b6
LeBalz <lebalz@outlook.com>
2023-11-17 16:01:40 +0100

    use component in user settings

--- src/components/User/index.tsx
+++ src/components/User/index.tsx
@@ -17,6 +17,7 @@ import { ApiState } from '@site/src/stores/iStore';
 import Translate, { translate } from '@docusaurus/Translate';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import _ from 'lodash';
+import ICal from '../iCal';
 
 
 interface Props {
@@ -143,100 +144,7 @@ const User = observer((props: Props) => {
                     />
                 </dt>
                 <dd>
-                    <div className={clsx(styles.icalSettings)}>
-                        <div className={clsx('card', styles.personal, styles.card)}>
-                            <div className={clsx('card__header')}>
-                                <h5>
-                                    <Translate id="ical.section.personal" description='personal ical sync address'>
-                                        Persönlicher Kalender
-                                    </Translate>
-                                </h5>
-                            </div>
-                            <div className={clsx('card__body')}>
-                                    <div className={clsx(styles.icalButtons)}>
-                                        <Button
-                                            href={`https://outlook.office.com/owa?path=%2Fcalendar%2Faction%2Fcompose&rru=addsubscription&url=${EVENTS_API}/ical/${currentLocale}/${user.icalUrl}&name=${translate({ message: 'GBSL', id: 'user.ical.outlook.calendar-name', description: 'Name of the calendar in Outlook' })}`}
-                                            target='_blank'
-                                            text={translate({ message: 'Outlook', id: 'user.ical.outlook-button.text', description: 'Button text for adding the calendar to Outlook' })}
-                                            title={translate({ message: 'Abonniere den Kalender in Outlook', id: 'user.ical.outlook-button.title', description: 'Button text for adding the calendar to Outlook' })}
-                                            icon={mdiMicrosoftOutlook}
-                                            color={'primary'}
-                                            size={SIZE_S}
-                                        />
-                                        <Button
-                                            onClick={() => userStore.createIcs()}
-                                            text={translate({ id: 'user.ical.sync-button.text', message: 'Sync', description: 'Button text for (re)syncing the calendar' })}
-                                            title={translate({ id: 'user.ical.sync-button.title', message: 'Synchronisiere meinen Kalender', description: 'Button (hover) title for (re)syncing the calendar' })}
-                                            icon={mdiSync}
-                                            apiState={userStore.apiStateFor('createIcs')}
-                                            size={SIZE_S}
-                                            disabled={userStore.apiStateFor('createIcs') === ApiState.LOADING}
-                                        />
-                                    </div>
-                                    <div className={clsx(styles.ical)}>
-                                        {user.icalUrl && `${EVENTS_API}/ical/${currentLocale}/${user.icalUrl}`}
-                                    </div>
-                            </div>
-                        </div>
-                        <div className={clsx('card', styles.card)}>
-                            <div className={clsx('card__header')}>
-                                <h5>
-                                    <Translate id="ical.section.departments" description='departments ical sync address'>
-                                        Abteilungs-Kalender
-                                    </Translate>
-                                </h5>
-                            </div>
-                            <div className={clsx('card__body', styles.icalWrapper)}>
-                                {_.orderBy(untisStore.classes, ['name'], ['asc']).map(c => (
-                                    <div className={clsx(styles.publicIcal)} key={c.name}>
-                                        <div className={clsx(styles.icalButton)}>
-                                            <Button
-                                                href={`https://outlook.office.com/owa?path=%2Fcalendar%2Faction%2Fcompose&rru=addsubscription&url=${EVENTS_API}/ical/${currentLocale}/${c.name}.ics&name=${c.displayName}`}
-                                                target='_blank'
-                                                text={c.displayName}
-                                                title={translate({ message: 'Abonniere den Kalender in Outlook', id: 'user.ical.outlook-button.title', description: 'Button text for adding the calendar to Outlook' })}
-                                                icon={mdiMicrosoftOutlook}
-                                                size={SIZE_S}
-                                                color={c.department.color}
-                                            />
-                                        </div>
-                                        <div className={clsx(styles.ical)}>
-                                            {user.icalUrl && `${EVENTS_API}/ical/${currentLocale}/${c.name}.ics`}
-                                        </div>
-                                    </div>
-                                ))}
-                            </div>
-                        </div>
-                        <div className={clsx('card', styles.card)}>
-                            <div className={clsx('card__header')}>
-                                <h5>
-                                    <Translate id="ical.section.classes" description='classes ical sync address'>
-                                        Klassen Kalender
-                                    </Translate>
-                                </h5>
-                            </div>
-                            <div className={clsx('card__body', styles.icalWrapper)}>
-                                {_.orderBy(departmentStore.departments, ['name'], ['asc']).map(d => (
-                                    <div className={clsx(styles.publicIcal)} key={d.name}>
-                                        <div className={clsx(styles.icalButton)}>
-                                            <Button
-                                                href={`https://outlook.office.com/owa?path=%2Fcalendar%2Faction%2Fcompose&rru=addsubscription&url=${EVENTS_API}/ical/${currentLocale}/${d.name.replaceAll('/', '_')}.ics&name=${d.name}`}
-                                                target='_blank'
-                                                text={d.name}
-                                                color={d.color}
-                                                title={translate({ message: 'Abonniere den Kalender in Outlook', id: 'user.ical.outlook-button.title', description: 'Button text for adding the calendar to Outlook' })}
-                                                icon={mdiMicrosoftOutlook}
-                                                size={SIZE_S}
-                                            />
-                                        </div>
-                                        <div className={clsx(styles.ical)}>
-                                            {user.icalUrl && `${EVENTS_API}/ical/${currentLocale}/${d.name.replaceAll('/', '_')}.ics`}
-                                        </div>
-                                    </div>
-                                ))}
-                            </div>
-                        </div>
-                    </div>
+                    <ICal />
                 </dd>
             </DefinitionList>
         </div>
