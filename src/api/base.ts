7e78c13f2bb95f99882112a6a393d66fefb7751c
LeBalz <lebalz@outlook.com>
2024-02-14 00:23:08 +0100

    auto-redirect user to login page

--- src/api/base.ts
+++ src/api/base.ts
@@ -1,7 +1,7 @@
 import axios, { InternalAxiosRequestConfig } from 'axios';
 import { EVENTS_API, apiConfig, TENANT_ID } from '../authConfig';
 import siteConfig from '@generated/docusaurus.config';
-import { PublicClientApplication } from '@azure/msal-browser';
+import { InteractionRequiredAuthError, PublicClientApplication } from '@azure/msal-browser';
 import { msalInstance } from '../theme/Root';
 const { TEST_USERNAME } = siteConfig.customFields as { TEST_USERNAME?: string };
 
@@ -30,14 +30,33 @@ export const setupAxios = () => {
             // This will only return a non-null value if you have logic somewhere else that calls the setActiveAccount API
             // --> @see src/theme/Root.tsx
             const activeAccount = msalInstance.getActiveAccount();
+            console.log('activeAccount', activeAccount);    
             if (activeAccount) {
-                const accessTokenResponse = await msalInstance.acquireTokenSilent({
-                    scopes: apiConfig.scopes,
-                    account: activeAccount
-                });
-                const accessToken = accessTokenResponse.accessToken;
-                if (config.headers && accessToken) {
-                    config.headers['Authorization'] = 'Bearer ' + accessToken;
+                try {
+                    const accessTokenResponse = await msalInstance.acquireTokenSilent({
+                        scopes: apiConfig.scopes,
+                        account: activeAccount
+                    });
+                    const accessToken = accessTokenResponse.accessToken;
+                    if (config.headers && accessToken) {
+                        config.headers['Authorization'] = 'Bearer ' + accessToken;
+                    }
+                } catch (e) {
+                    delete config.headers['Authorization'];
+                    if (e instanceof InteractionRequiredAuthError) {
+                        // If there are no cached tokens, or the cached tokens are expired, then the user will need to interact 
+                        // with the page to get a new token.
+                        console.log('User interaction required to get a new token.', e);
+                        // hacky way to get the user to log in again - only happens on firefox when
+                        // the default "no 3dparty cookies" setting is active
+                        const msalKeys = Object.keys(localStorage).filter((k) => k.startsWith('msal.'));
+                        msalKeys.forEach((k) => localStorage.removeItem(k));
+                        // proceed with the login
+                        await msalInstance.acquireTokenRedirect({
+                            scopes: apiConfig.scopes,
+                            account: activeAccount
+                        });
+                    }
                 }
             } else {
                 /*
