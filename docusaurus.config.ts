74010f06b24778b50d4e6d04c368931150c14261
LeBalz <lebalz@outlook.com>
2024-04-29 14:02:49 +0200

    fix edit url

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -148,8 +148,8 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-            remarkPlugins: [        
+            'https://github.com/lebalz/events-app/edit/main/',
+          remarkPlugins: [        
               [strongPlugin, { className: 'boxed'}],
               [
                   deflistPlugin,
@@ -180,7 +180,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/lebalz/events-app/edit/main/',
         },
         theme: {
           customCss: [
