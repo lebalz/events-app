0c7d0dc89b2d181c5206a39dc2675052743cd219
LeBalz <lebalz@outlook.com>
2024-10-31 16:32:45 +0100

    use nested popup

--- src/components/Event/EventFields/IsValid/index.tsx
+++ src/components/Event/EventFields/IsValid/index.tsx
@@ -3,7 +3,6 @@ import clsx from 'clsx';
 
 import { observer } from 'mobx-react-lite';
 import sharedStyles from '../styles.module.scss';
-import styles from './styles.module.scss';
 
 import { ReadonlyProps } from '../iEventField';
 import Popup from 'reactjs-popup';
@@ -11,8 +10,7 @@ import Button from '../../../shared/Button';
 import { mdiCloseCircle, mdiAlertCircle, mdiInformation, mdiCheckCircle } from '@mdi/js';
 import { Icon, SIZE_S } from '../../../shared/icons';
 import { ValidState } from '@site/src/models/Event';
-import EventOverviewSmall from '../../EventOverviewSmall';
-import { translate } from '@docusaurus/Translate';
+import PopupContent from './PopupContent';
 
 const StateIcon: { [key in ValidState]: string } = {
     [ValidState.Valid]: mdiCheckCircle,
@@ -30,9 +28,6 @@ const StateColor: { [key in ValidState]: string } = {
 
 const IsValid = observer((props: ReadonlyProps) => {
     const { event } = props;
-    const errors = event._errors?.details || [];
-    const warnings = event.meta?.warningsReviewed ? [] : event.meta?.warnings || [];
-    const infos = event.meta?.infosReviewed ? [] : event.meta?.infos || [];
     React.useEffect(() => {
         event.triggerInitialValidation();
     }, [event.id]);
@@ -46,6 +41,9 @@ const IsValid = observer((props: ReadonlyProps) => {
                 ''
             ) : (
                 <Popup
+                    closeOnEscape
+                    closeOnDocumentClick
+                    nested
                     trigger={
                         <span>
                             <Button
@@ -62,54 +60,8 @@ const IsValid = observer((props: ReadonlyProps) => {
                     }
                     position="right center"
                     on="click"
-                    closeOnDocumentClick
                 >
-                    {errors.length > 0 && (
-                        <ul className={clsx(sharedStyles.errors)}>
-                            {errors.map((error, index) => (
-                                <li key={index}>{error.message}</li>
-                            ))}
-                        </ul>
-                    )}
-                    {warnings.length > 0 && (
-                        <ul className={clsx(sharedStyles.warnings)}>
-                            {warnings.map((warning, index) => (
-                                <li key={index}>{warning}</li>
-                            ))}
-                        </ul>
-                    )}
-                    {event.overlappingEvents.length > 0 && (
-                        <div className={clsx(styles.conflicts)}>
-                            <h3>
-                                {translate(
-                                    {
-                                        id: 'event.isValid.conflicts',
-                                        description:
-                                            'Conflicts between events that are overlapping and concerning the same event',
-                                        message: '{count} Konflikte'
-                                    },
-                                    { count: event.overlappingEvents.length }
-                                )}
-                            </h3>
-                            <div className={clsx(styles.overlappingEvents)}>
-                                <EventOverviewSmall event={event} className={clsx(styles.main)} />
-                                {event.overlappingEvents.map((overlap, idx) => (
-                                    <EventOverviewSmall
-                                        event={overlap}
-                                        key={overlap.id}
-                                        className={clsx(styles.overview)}
-                                    />
-                                ))}
-                            </div>
-                        </div>
-                    )}
-                    {infos.length > 0 && (
-                        <ul className={clsx(sharedStyles.infos)}>
-                            {infos.map((info, index) => (
-                                <li key={index}>{info}</li>
-                            ))}
-                        </ul>
-                    )}
+                    <PopupContent event={event} />
                 </Popup>
             )}
         </div>
