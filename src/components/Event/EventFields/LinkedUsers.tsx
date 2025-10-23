25c1b8444fb3fbc257ca5d6c4d6526bee8c9f616
bh0fer <lebalz@outlook.com>
2025-10-23 13:45:16 +0200

    update linked users text

--- src/components/Event/EventFields/LinkedUsers.tsx
+++ src/components/Event/EventFields/LinkedUsers.tsx
@@ -5,6 +5,7 @@ import styles from './styles.module.scss';
 import { observer } from 'mobx-react-lite';
 import { Props as CommonProps } from './iEventField';
 import Badge from '@site/src/components/shared/Badge';
+import { translate } from '@docusaurus/Translate';
 
 interface Props extends CommonProps {
     isEditGrid?: boolean /** true when at least one element of the grid is edited */;
@@ -27,17 +28,44 @@ const LinkedUsers = observer((props: Props) => {
             )}
         >
             <div className={clsx(styles.tags)}>
-                {event.linkedUsers.map((u, idx) => {
-                    return (
-                        <Badge
-                            key={idx}
-                            text={u.displayName}
-                            title={u.email}
-                            color={LINKED_USER_COLOR}
-                            className={clsx(styles.badge)}
-                        />
-                    );
-                })}
+                {event.linkedUserIds.size > 0 && (
+                    <>
+                        {event.linkedUsers.length > 0 ? (
+                            event.linkedUsers.map((u, idx) => {
+                                return (
+                                    <Badge
+                                        key={idx}
+                                        text={u.displayName}
+                                        title={u.email}
+                                        color={LINKED_USER_COLOR}
+                                        className={clsx(styles.badge)}
+                                    />
+                                );
+                            })
+                        ) : (
+                            <Badge
+                                text={translate(
+                                    {
+                                        id: 'event.linkedUserIds.size',
+                                        message: '${count} Personen verknüpft',
+                                        description: 'Badge text for linked users'
+                                    },
+                                    { count: event.linkedUserIds.size }
+                                )}
+                                title={translate(
+                                    {
+                                        id: 'event.linkedUserIds.size.title',
+                                        message: 'Es sind ${count} Personen mit diesem Ereignis verknüpft.',
+                                        description: 'Badge title for linked users'
+                                    },
+                                    { count: event.linkedUserIds.size }
+                                )}
+                                color={LINKED_USER_COLOR}
+                                className={clsx(styles.badge)}
+                            />
+                        )}
+                    </>
+                )}
             </div>
         </div>
     );
