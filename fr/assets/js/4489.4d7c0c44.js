cf2a90feb7d0d803b0790744c7556f37053dfbdc
bh0fer <lebalz@outlook.com>
2026-03-03 07:16:41 +0000

    Deploy website - based on fe36147663c17c62b04c5c3ff53727155b6b9637

---
+++ fr/assets/js/4489.4d7c0c44.js
@@ -0,0 +1,44 @@
+try{var e="undefined"!=typeof window?window:"undefined"!=typeof global?global:"undefined"!=typeof globalThis?globalThis:"undefined"!=typeof self?self:{};e.SENTRY_RELEASE={id:"fe36147663c17c62b04c5c3ff53727155b6b9637"};var t=(new e.Error).stack;t&&(e._sentryDebugIds=e._sentryDebugIds||{},e._sentryDebugIds[t]="e8cf0eb9-7725-49f0-8370-62cdeb0edb83",e._sentryDebugIdIdentifier="sentry-dbid-e8cf0eb9-7725-49f0-8370-62cdeb0edb83")}catch(e){}"use strict";(self.webpackChunkevents_app=self.webpackChunkevents_app||[]).push([["4489"],{25871:function(e,t,a){function r(e,t){e.accDescr&&t.setAccDescription?.(e.accDescr),e.accTitle&&t.setAccTitle?.(e.accTitle),e.title&&t.setDiagramTitle?.(e.title)}a.d(t,{S:()=>r}),(0,a(40797).K2)(r,"populateCommonDb")},16992:function(e,t,a){a.d(t,{diagram:()=>E});var r=a(73590),i=a(25871),n=a(13226),l=a(58365),s=a(40797),o=a(78731),c={showLegend:!0,ticks:5,max:null,min:0,graticule:"circle"},d={axes:[],curves:[],options:c},g=structuredClone(d),u=l.UI.radar,p=(0,s.K2)(()=>(0,n.$t)({...u,...(0,l.zj)().radar}),"getConfig"),h=(0,s.K2)(()=>g.axes,"getAxes"),x=(0,s.K2)(()=>g.curves,"getCurves"),f=(0,s.K2)(()=>g.options,"getOptions"),m=(0,s.K2)(e=>{g.axes=e.map(e=>({name:e.name,label:e.label??e.name}))},"setAxes"),$=(0,s.K2)(e=>{g.curves=e.map(e=>({name:e.name,label:e.label??e.name,entries:y(e.entries)}))},"setCurves"),y=(0,s.K2)(e=>{if(void 0==e[0].axis)return e.map(e=>e.value);let t=h();if(0===t.length)throw Error("Axes must be populated before curves for reference entries");return t.map(t=>{let a=e.find(e=>e.axis?.$refText===t.name);if(void 0===a)throw Error("Missing entry for axis "+t.label);return a.value})},"computeCurveEntries"),b={getAxes:h,getCurves:x,getOptions:f,setAxes:m,setCurves:$,setOptions:(0,s.K2)(e=>{let t=e.reduce((e,t)=>(e[t.name]=t,e),{});g.options={showLegend:t.showLegend?.value??c.showLegend,ticks:t.ticks?.value??c.ticks,max:t.max?.value??c.max,min:t.min?.value??c.min,graticule:t.graticule?.value??c.graticule}},"setOptions"),getConfig:p,clear:(0,s.K2)(()=>{(0,l.IU)(),g=structuredClone(d)},"clear"),setAccTitle:l.SV,getAccTitle:l.iN,setDiagramTitle:l.ke,getDiagramTitle:l.ab,getAccDescription:l.m7,setAccDescription:l.EI},v=(0,s.K2)(e=>{(0,i.S)(e,b);let{axes:t,curves:a,options:r}=e;b.setAxes(t),b.setCurves(a),b.setOptions(r)},"populate"),w={parse:(0,s.K2)(async e=>{let t=await (0,o.qg)("radar",e);s.Rm.debug(t),v(t)},"parse")},C=(0,s.K2)((e,t,a,i)=>{let n=i.db,l=n.getAxes(),s=n.getCurves(),o=n.getOptions(),c=n.getConfig(),d=n.getDiagramTitle(),g=M((0,r.D)(t),c),u=o.max??Math.max(...s.map(e=>Math.max(...e.entries))),p=o.min,h=Math.min(c.width,c.height)/2;T(g,l,h,o.ticks,o.graticule),K(g,l,h,c),L(g,l,s,p,u,o.graticule,c),I(g,s,o.showLegend,c),g.append("text").attr("class","radarTitle").text(d).attr("x",0).attr("y",-c.height/2-c.marginTop)},"draw"),M=(0,s.K2)((e,t)=>{let a=t.width+t.marginLeft+t.marginRight,r=t.height+t.marginTop+t.marginBottom,i={x:t.marginLeft+t.width/2,y:t.marginTop+t.height/2};return e.attr("viewbox",`0 0 ${a} ${r}`).attr("width",a).attr("height",r),e.append("g").attr("transform",`translate(${i.x}, ${i.y})`)},"drawFrame"),T=(0,s.K2)((e,t,a,r,i)=>{if("circle"===i)for(let t=0;t<r;t++){let i=a*(t+1)/r;e.append("circle").attr("r",i).attr("class","radarGraticule")}else if("polygon"===i){let i=t.length;for(let n=0;n<r;n++){let l=a*(n+1)/r,s=t.map((e,t)=>{let a=2*t*Math.PI/i-Math.PI/2,r=l*Math.cos(a),n=l*Math.sin(a);return`${r},${n}`}).join(" ");e.append("polygon").attr("points",s).attr("class","radarGraticule")}}},"drawGraticule"),K=(0,s.K2)((e,t,a,r)=>{let i=t.length;for(let n=0;n<i;n++){let l=t[n].label,s=2*n*Math.PI/i-Math.PI/2;e.append("line").attr("x1",0).attr("y1",0).attr("x2",a*r.axisScaleFactor*Math.cos(s)).attr("y2",a*r.axisScaleFactor*Math.sin(s)).attr("class","radarAxisLine"),e.append("text").text(l).attr("x",a*r.axisLabelFactor*Math.cos(s)).attr("y",a*r.axisLabelFactor*Math.sin(s)).attr("class","radarAxisLabel")}},"drawAxes");function L(e,t,a,r,i,n,l){let s=t.length,o=Math.min(l.width,l.height)/2;a.forEach((t,a)=>{if(t.entries.length!==s)return;let c=t.entries.map((e,t)=>{let a=2*Math.PI*t/s-Math.PI/2,n=k(e,r,i,o);return{x:n*Math.cos(a),y:n*Math.sin(a)}});"circle"===n?e.append("path").attr("d",A(c,l.curveTension)).attr("class",`radarCurve-${a}`):"polygon"===n&&e.append("polygon").attr("points",c.map(e=>`${e.x},${e.y}`).join(" ")).attr("class",`radarCurve-${a}`)})}function k(e,t,a,r){return r*(Math.min(Math.max(e,t),a)-t)/(a-t)}function A(e,t){let a=e.length,r=`M${e[0].x},${e[0].y}`;for(let i=0;i<a;i++){let n=e[(i-1+a)%a],l=e[i],s=e[(i+1)%a],o=e[(i+2)%a],c={x:l.x+(s.x-n.x)*t,y:l.y+(s.y-n.y)*t},d={x:s.x-(o.x-l.x)*t,y:s.y-(o.y-l.y)*t};r+=` C${c.x},${c.y} ${d.x},${d.y} ${s.x},${s.y}`}return`${r} Z`}function I(e,t,a,r){if(!a)return;let i=(r.width/2+r.marginRight)*3/4,n=-(3*(r.height/2+r.marginTop))/4;t.forEach((t,a)=>{let r=e.append("g").attr("transform",`translate(${i}, ${n+20*a})`);r.append("rect").attr("width",12).attr("height",12).attr("class",`radarLegendBox-${a}`),r.append("text").attr("x",16).attr("y",0).attr("class","radarLegendText").text(t.label)})}(0,s.K2)(L,"drawCurves"),(0,s.K2)(k,"relativeRadius"),(0,s.K2)(A,"closedRoundCurve"),(0,s.K2)(I,"drawLegend");var S=(0,s.K2)((e,t)=>{let a="";for(let r=0;r<e.THEME_COLOR_LIMIT;r++){let i=e[`cScale${r}`];a+=`
+		.radarCurve-${r} {
+			color: ${i};
+			fill: ${i};
+			fill-opacity: ${t.curveOpacity};
+			stroke: ${i};
+			stroke-width: ${t.curveStrokeWidth};
+		}
+		.radarLegendBox-${r} {
+			fill: ${i};
+			fill-opacity: ${t.curveOpacity};
+			stroke: ${i};
+		}
+		`}return a},"genIndexStyles"),D=(0,s.K2)(e=>{let t=(0,l.P$)(),a=(0,l.zj)(),r=(0,n.$t)(t,a.themeVariables),i=(0,n.$t)(r.radar,e);return{themeVariables:r,radarOptions:i}},"buildRadarStyleOptions"),E={parser:w,db:b,renderer:{draw:C},styles:(0,s.K2)(({radar:e}={})=>{let{themeVariables:t,radarOptions:a}=D(e);return`
+	.radarTitle {
+		font-size: ${t.fontSize};
+		color: ${t.titleColor};
+		dominant-baseline: hanging;
+		text-anchor: middle;
+	}
+	.radarAxisLine {
+		stroke: ${a.axisColor};
+		stroke-width: ${a.axisStrokeWidth};
+	}
+	.radarAxisLabel {
+		dominant-baseline: middle;
+		text-anchor: middle;
+		font-size: ${a.axisLabelFontSize}px;
+		color: ${a.axisColor};
+	}
+	.radarGraticule {
+		fill: ${a.graticuleColor};
+		fill-opacity: ${a.graticuleOpacity};
+		stroke: ${a.graticuleColor};
+		stroke-width: ${a.graticuleStrokeWidth};
+	}
+	.radarLegendText {
+		text-anchor: start;
+		font-size: ${a.legendFontSize}px;
+		dominant-baseline: hanging;
+	}
+	${S(t,a)}
+	`},"styles")}}}]);
+//# sourceMappingURL=4489.4d7c0c44.js.map
